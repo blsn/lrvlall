8630c2922262dbf3874cf3bccf6fd011e4316254
Moshe Balasiano <mbalasiano@leolane.com>
2020-04-18 16:42:11 +0300

    blade templating

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
 
 Auth::routes();
 
