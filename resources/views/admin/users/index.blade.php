c011ea6972c3370317a4cf95ab6fa162c45d291c
Moshe Balasiano <mbalasiano@leolane.com>
2020-04-19 13:10:42 +0300

    table responsive

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,39 +7,41 @@
             <div class="card">
                 <div class="card-header">Users</div>
                 <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Role</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($users as $user)
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
                                 <tr>
-                                    <th scope="row">{{$user->id}}</th>
-                                    <td>{{$user->name}}</td>
-                                    <td>{{$user->email}}</td>
-                                    <td>{{implode(', ', $user->roles()->get()->pluck('name')->toArray())}}</td>
-                                    <td>
-                                        @can('edit-users')
-                                            <a href="{{route('admin.users.edit', $user)}}"><button type="button" class="btn btn-primary float-left">Edit</button></a>
-                                        @endcan
-                                        @can('delete-users')
-                                            <form action="{{route('admin.users.destroy', $user->id)}}" method="POST" class="float-left">
-                                                @csrf
-                                                {{method_field('DELETE')}}
-                                                <button type="submit" class="btn btn-warning">Delete</button>
-                                            </form>
-                                        @endcan
-                                    </td>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Role</th>
+                                    <th scope="col">Action</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td>{{$user->id}}</td>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+                                        <td>{{implode(', ', $user->roles()->get()->pluck('name')->toArray())}}</td>
+                                        <td>
+                                            @can('edit-users')
+                                                <a href="{{route('admin.users.edit', $user)}}"><button type="button" class="btn btn-primary float-left">Edit</button></a>
+                                            @endcan
+                                            @can('delete-users')
+                                                <form action="{{route('admin.users.destroy', $user->id)}}" method="POST" class="float-left">
+                                                    @csrf
+                                                    {{method_field('DELETE')}}
+                                                    <button type="submit" class="btn btn-warning">Delete</button>
+                                                </form>
+                                            @endcan
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
