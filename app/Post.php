43afc257cb4681c113dde3938e65070654f7f5fc
Moshe Balasiano <mbalasiano@leolane.com>
2020-04-25 15:00:31 +0300

    model relationships

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $table = 'posts'; // table name is the plural form of the model name, otherwise protected explicit table name
+    protected $table = 'posts'; // table name plural of model name, otherwise protected explicit table name
     public $primaryKey = 'id'; // primary key
     public $timestamps = true; // timestamps
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
