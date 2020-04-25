83b579671af10523052c46606fe30704fa1ea6e5
Moshe Balasiano <mbalasiano@leolane.com>
2020-04-25 13:09:49 +0300

    change ‘home’ to ‘dashboard’

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -44,7 +44,7 @@ class LoginController extends Controller
             $this->redirectTo = route('admin.users.index');
             return $this->redirectTo;
         }
-        $this->redirectTo = route('home');
+        $this->redirectTo = route('dashboard');
         return $this->redirectTo;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::resource('posts', 'PostsController');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
 // Route::resource('/admin/users', 'Admin\UsersController', ['except' => ['show', 'create', 'store']]);
 
